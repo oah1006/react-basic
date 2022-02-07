a085bac98e4f60d39a2e97c6955bf4eb9720a1fe
oah1006 <bnhao10062001@gmail.com>
2022-02-07 20:42:34 +0700

    reactjs: study state

---
+++ src/views/example/MyComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+
+class MyComponent extends React.Component {
+
+    state = {
+        name: 'Ronaldo',
+        age: 20,
+
+    }
+
+    /* 
+    JSX -> return block
+    fragment
+    
+    */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }   
+
+    render() {
+        return (
+            <>
+                <div className="first">
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+                    my name is {this.state.name}
+                </div>
+                <div className="second">
+                    My age is {this.state.age}
+                </div>
+            </>
+        )
+    }
+
+}
+
+
+export default MyComponent;
