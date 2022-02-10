0e664cb2fbbc0846477b2e40dbb2f2b8cd163969
oah1006 <bnhao10062001@gmail.com>
2022-02-10 17:55:23 +0700

    reactJs: create child Components

---
+++ src/views/example/ChildComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+
+class ChildComponent extends React.Component {
+
+    state = {
+        firstName: '',
+        lastName: '',
+    }
+
+    /* 
+    JSX -> return block
+    fragment
+    
+    */
+
+    handleChangeFirstName = (event) => {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    handleChangeLastName = (event) => {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('>> Check data input: ', this.state);
+    }
+
+    // re-render
+    render() {
+        console.log('>>> check props: ', this.props)
+        // let name = this.props.name;
+        // let age = this.props.age;
+
+        let {name, age} = this.props;
+        return (
+            <>
+                <div>Child Component: {name} - {age}</div>
+            </>
+        )
+    }
+
+}
+
+
+export default ChildComponent;
