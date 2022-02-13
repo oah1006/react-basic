0737755abc759250e4e8607660291c50a2c771cd
oah1006 <bnhao10062001@gmail.com>
2022-02-13 13:25:42 +0700

    reactJs: create toggle datas

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,49 +1,74 @@
 import React from 'react';
 
-// class ChildComponent extends React.Component {
-//     // re-render
-//     render() {
-//         console.log('>>> call props: ', this.props)
-//         let { name, age, address, arrJobs } = this.props;
-//         let a = '';
-//         return (
-//             <>
-//                <div className="job-lists">
-//                    {
-//                         a = arrJobs.map((item, index) => {
+class ChildComponent extends React.Component {
+
+    state = {
+        showJobs: false
+    }
+    
+    handleShowHide  = () => {
+        this.setState ({
+            showJobs: !this.state.showJobs
+        })
+    }
+
+    // re-render
+    render() {
+        let {arrJobs } = this.props;
+        let {showJobs} = this.state;
+
+        let check = showJobs === true ? 'showJobs = true' : 'showJobs = false';
+        console.log('>>> check conditional: ', check)
+        return (
+            <>
+                {showJobs === false ? 
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                :
+                    <>
+                        <div className="job-lists">
+                            {
+                                    arrJobs.map((item, index) => {
+                                        return (
+                                            <div key={item.id}>
+                                                {item.title} - {item.salary}
+                                            </div>
+                                        )
+                                })
+                            }
+                        </div>
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>hide</button>
+                        </div>
+                    </>
+                }
+            </>
+        )
+    }
+
+}
+
+// const ChildComponent = (props) => {
+//     let {arrJobs} = props;
+//     return (
+//         <>
+//             <div className="job-lists">
+//                 {   
+//                     arrJobs.map((item, index) => {
+//                         if (item.salary >= 500) {
 //                             return (
 //                                 <div key={item.id}>
 //                                     {item.title} - {item.salary}
 //                                 </div>
 //                             )
-//                        })
-//                    }
-//                    {console.log('>> check map array: ', a)}
-//                </div>
-//             </>
-//         )
-//     }
-
+//                         }
+//                     })
+//                 }
+//             </div>
+//         </>
+//     )
 // }
-
-const ChildComponent = (props) => {
-    let {arrJobs} = props;
-    return (
-        <>
-            <div className="job-lists">
-                {
-                    arrJobs.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {item.title} - {item.salary}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        </>
-    )
-}
 
 
 export default ChildComponent;
